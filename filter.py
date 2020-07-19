6a12f5fb6dd26075d44b5ba0266e99045fe2face
Derek Dang <dqdang17@gmail.com>
2020-07-19 15:47:26 -0700

    Formatting

--- filter.py
+++ filter.py
@@ -3,11 +3,20 @@ import os
 import praw
 import time
 
-R_USERNAME = os.environ["USERNAME"]
-R_PASSWORD = os.environ["PASSWORD"]
-R_CID = os.environ["R_CID"]
-R_SECRET = os.environ["R_SECRET"]
-R_SUBREDDIT = os.environ["SUBREDDIT"]
+try:
+    R_USERNAME = os.environ["USERNAME"]
+    R_PASSWORD = os.environ["PASSWORD"]
+    R_CID = os.environ["R_CID"]
+    R_SECRET = os.environ["R_SECRET"]
+    R_SUBREDDIT = os.environ["SUBREDDIT"]
+
+except KeyError as e:
+    with open("creds.txt", "r") as f:
+        R_USERNAME = f.readline().strip()
+        R_PASSWORD = f.readline().strip()
+        R_CID = f.readline().strip()
+        R_SECRET = f.readline().strip()
+        R_SUBREDDIT = f.readline().strip()
 
 
 def get_subreddits(args):
@@ -18,6 +27,7 @@ def get_subreddits(args):
     with open("subreddits.txt", "r") as f:
         lines = f.readlines()
         for line in lines:
+            line = line.strip()
             if len(line) > 0:
                 subreddits += line + "+"
     subreddits = subreddits[:-2]
@@ -59,10 +69,12 @@ if __name__ == '__main__':
         with open(write_to, "r") as f:
             submitted = f.read()
             submitted = submitted.split("\n")
-            submitted = list(filter(None, submitted))
+            submitted = set(filter(None, submitted))
 
     try:
-        subreddit = reddit.subreddit(get_subreddits(args))
+        subreddits = get_subreddits(args)
+        print(subreddits)
+        subreddit = reddit.subreddit(subreddits)
         keywords = get_keywords(args)
         for submission in subreddit.stream.submissions():
             print(submission.title)
@@ -72,7 +84,9 @@ if __name__ == '__main__':
                         try:
                             cross_post = submission.crosspost(subreddit=R_SUBREDDIT,
                                                               send_replies=False)
+                            submitted.add(submission.id)
                             break
+
                         except:
                             print("Waiting... 0 minutes")
                             time.sleep(300)
@@ -83,12 +97,15 @@ if __name__ == '__main__':
                             print("Done waiting... 15 minutes")
                             cross_post = submission.crosspost(subreddit=R_SUBREDDIT,
                                                               send_replies=False)
+                            submitted.add(submission.id)
                             break
-                        submitted.append(submission.id)
+
     except KeyboardInterrupt:
-        with open(write_to, "w") as f:
+        with open(write_to, "w+") as f:
             for post_id in submitted:
                 f.write(post_id + "\n")
+                print(post_id)
+
     except Exception as e:
         print(e)
         exit()
